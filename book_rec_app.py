72fa9525d732b87e70c022bab229020e5dfa38b0
Bernardo Vargas da Motta Alves <bernardovma@gmail.com>
2024-06-19 10:25:22 -0300

    merge das outras branches com projeto completo

---
+++ book_rec_app.py
@@ -0,0 +1,203 @@
+import docker
+from vespa.io import VespaResponse, VespaQueryResponse
+from vespa.package import (
+    ApplicationPackage,
+    Field,
+    Schema,
+    Document,
+    HNSW,
+    RankProfile,
+    Component,
+    Parameter,
+    FieldSet,
+    GlobalPhaseRanking,
+    Function,
+)
+from vespa.deployment import VespaDocker
+import pandas as pd
+import numpy as np
+
+class VespaApp:
+    def __init__(self):
+        self.app = self.vespa_docker_deploy()
+        self.app = self.start_vespa(self.app)
+
+    def create_package(self):
+        package = ApplicationPackage(
+            name="hybridsearchbv",
+            schema=[
+                Schema(
+                    name="doc",
+                    document=Document(
+                        fields=[
+                            Field(name="id", type="string", indexing=["summary"]),
+                            Field(
+                                name="title",
+                                type="string",
+                                indexing=["index", "summary"],
+                                index="enable-bm25",
+                            ),
+                            Field(
+                                name="authors",
+                                type="string",
+                                indexing=["index", "summary"],
+                                index="enable-bm25",
+                            ),
+                            Field(
+                                name="description",
+                                type="string",
+                                indexing=["index", "summary"],
+                                index="enable-bm25",
+                            ),
+                            Field(
+                                name="categories",
+                                type="string",
+                                indexing=["index", "summary"],
+                                index="enable-bm25",
+                            ),
+                            Field(
+                                name="embedding",
+                                type="tensor<float>(x[384])",
+                                indexing=[
+                                    'input description . " " . input categories',
+                                    "embed",
+                                    "index",
+                                    "attribute",
+                                ],
+                                ann=HNSW(distance_metric="angular"),
+                                is_document_field=False,
+                            ),
+                        ]
+                    ),
+                    fieldsets=[FieldSet(name="default", fields=["title", "authors", "description", "categories"])],
+                    rank_profiles=[
+                        RankProfile(
+                            name="bm25",
+                            inputs=[("query(q)", "tensor<float>(x[384])")],
+                            functions=[
+                                Function(name="bm25sum", expression="bm25(description) + bm25(categories)")
+                            ],
+                            first_phase="bm25sum",
+                        ),
+                        RankProfile(
+                            name="semantic",
+                            inputs=[("query(q)", "tensor<float>(x[384])")],
+                            first_phase="closeness(field, embedding)",
+                        ),
+                        RankProfile(
+                            name="fusion",
+                            inherits="bm25",
+                            inputs=[("query(q)", "tensor<float>(x[384])")],
+                            first_phase="closeness(field, embedding)",
+                            global_phase=GlobalPhaseRanking(
+                                expression="reciprocal_rank_fusion(bm25sum, closeness(field, embedding))",
+                                rerank_count=1000,
+                            ),
+                        ),
+                    ],
+                )
+            ],
+            components=[
+                Component(
+                    id="e5",
+                    type="hugging-face-embedder",
+                    parameters=[
+                        Parameter(
+                            "transformer-model",
+                            {
+                                "url": "https://github.com/vespa-engine/sample-apps/raw/master/simple-semantic-search/model/e5-small-v2-int8.onnx"
+                            },
+                        ),
+                        Parameter(
+                            "tokenizer-model",
+                            {
+                                "url": "https://raw.githubusercontent.com/vespa-engine/sample-apps/master/simple-semantic-search/model/tokenizer.json"
+                            },
+                        ),
+                    ],
+                )
+            ],
+        )
+
+        return package
+
+
+    def vespa_docker_deploy(self):
+        package = self.create_package()
+
+        vespa_docker = VespaDocker()
+        app = vespa_docker.deploy(application_package=package)
+
+        return app
+
+
+    def transform_row(self, row):
+        return {
+            "id": row["id"],
+            "fields": {"title": row["title"], "authors": row["authors"], "description": row["description"], "categories": row["categories"], "id": row["id"]},
+        }
+
+
+    def callback(self, response:VespaResponse, id:str):
+        if not response.is_successful():
+            print(f"Error when feeding document {id}: {response.get_json()}")
+
+
+    def hits_as_df(self, response, fields):
+        records = []
+        for hit in response.hits:
+            record = {}
+            for field in fields:
+                record[field] = hit['fields'].get(field, None)  
+            records.append(record)
+        return pd.DataFrame(records)
+
+
+    def start_vespa(self, app):
+        df = pd.read_csv("https://raw.githubusercontent.com/bernardovma/dados_livros/main/data.csv")
+        df['id'] = range(1, len(df) + 1)
+        df = df.fillna("")
+        vespa_feed = df.apply(self.transform_row, axis=1).tolist()
+
+        app.feed_iterable(vespa_feed, schema="doc", namespace="bookrec", callback=self.callback)
+
+        return app
+
+
+    def query_bm25(self, query, limit = 10):
+        with self.app.syncio(connections=12) as session:
+                response:VespaQueryResponse = session.query(
+                    yql=f"select * from sources * where userQuery() limit {limit}",
+                    query=query,
+                    ranking="bm25"
+                )
+                assert(response.is_successful())
+
+        return self.hits_as_df(response, ['id', 'title', 'authors', 'description', 'categories'])
+
+
+    def query_semantic(self, input_query):
+        with self.app.syncio(connections=12) as session:
+            query = input_query
+            response: VespaQueryResponse = session.query(
+                yql="select * from sources * where ({targetHits:1000}nearestNeighbor(embedding,q)) limit 10",
+                query=query,
+                ranking="semantic",
+                body={"input.query(q)": f"embed({query})"},
+            )
+            assert response.is_successful()
+        return self.hits_as_df(response, ['id', 'title', 'authors', 'description', 'categories'])
+    
+
+    def query_hybrid(self, input_query):
+        with self.app.syncio(connections=12) as session:
+            query = input_query
+            response: VespaQueryResponse = session.query(
+                yql="select * from sources * where userQuery() or ({targetHits:1000}nearestNeighbor(embedding,q)) limit 10",
+                query=query,
+                ranking="fusion",
+                body={"input.query(q)": f"embed({query})"},
+            )
+            assert response.is_successful()
+
+        return self.hits_as_df(response, ['id', 'title', 'authors', 'description', 'categories']) 
--- main.py
+++ main.py
@@ -1,75 +1,37 @@
-import pandas as pd
 from flask import Flask, render_template, request, send_from_directory
-import numpy as np
+import pandas as pd
+from book_rec_app import VespaApp
 
-app = Flask(__name__)
+app_flask = Flask(__name__)
+app_vespa = VespaApp()
 
-# Carregar dados dos livros
-try:
-    books_data = pd.read_csv("https://raw.githubusercontent.com/bernardovma/dados_livros/main/data.csv")
-except Exception as e:
-    print(f"Failed to load CSV data:\n{e}")
-    exit()
-    
-# Criar função para recomendar livros
-def recommend_books(book):
-    # Calcular relevância de cada livro
-    books_data['relevance'] = books_data['average_rating'] * books_data['ratings_count']
+empty_df = pd.DataFrame(columns=["id", "title", "authors", "description", "categories", "thumbnail"])
 
-    # Filtrar livros similares
-    similar_books = books_data[(books_data['authors'] == book['authors'].values[0]) | (books_data['categories'] == book['categories'].values[0])]
-    similar_books = similar_books[similar_books['title'] != book['title'].values[0]]
-
-    # Calcula NDCG score
-    similar_books['ndcg_score'] = similar_books['relevance'].rank(ascending=False, method='max') / np.log2(similar_books.index + 2)
-
-    # Ranking dos livros e escolha do 5 melhores
-    recommended_books = similar_books.sort_values(by='ndcg_score', ascending=False).head(5)
-
-    return recommended_books
-
-# Função para buscar livro
-def search_book(book_name):
-    book_name = book_name.strip().lower()
-    book = books_data.loc[books_data['title'].str.lower() == book_name]
-    if not book.empty:
-        book_info = f"Category: {book['categories'].values[0]}\n" \
-                    f"Year: {int(book['published_year'].values[0])}\n" \
-                    f"Rating: {book['average_rating'].values[0]}\n" \
-                    f"Pages: {int(book['num_pages'].values[0])}\n" \
-                    f"Description: {book['description'].values[0]}"
-        image_url = book['thumbnail'].values[0]
-        title = f"{book['title'].values[0]} - {book['authors'].values[0]}"
-        recommended_books = recommend_books(book)
-        if not recommended_books.empty:
-            list_books = []
-            for index, row in recommended_books.iterrows():
-                list_books.append([row['title'], row['thumbnail'], row['authors']])
-            recommended_books = list_books
-        else:
-            recommended_books = []
-    else:
-        book_info = "Book not found."
-        image_url = ""
-        title = ""
-        recommended_books = []
-
-    return title, book_info, image_url, recommended_books
-
-@app.route('/static/<path:path>')
+@app_flask.route('/static/<path:path>')
 def serve_static(path):
     return send_from_directory('static', path)
 
-@app.route("/")
+@app_flask.route("/")
 def index():
-    return render_template("index.html", title="", book_info="", image_url="", recommended_books="")
+    return render_template("index.html", recommended_books=empty_df)
 
-@app.route("/search", methods=["POST"])
+@app_flask.route("/search", methods=["POST"])
 def search():
-    book_name = request.form["book_name"]
-    title, book_info, image_url, recommended_books = search_book(book_name)
-    return render_template("index.html", title=title, book_info=book_info, 
-                           image_url=image_url, recommended_books=recommended_books)
+    query = request.form["query"]
+    rank_type = request.form.get("rank_type", "bm25") 
+
+    if rank_type == "bm25":
+        recommended_books = app_vespa.query_bm25(query)
+    elif rank_type == "semantic":
+        recommended_books = app_vespa.query_semantic(query)
+    elif rank_type == "hybrid":
+        recommended_books = app_vespa.query_hybrid(query)
+
+    df = pd.read_csv("https://raw.githubusercontent.com/bernardovma/dados_livros/main/data.csv")
+
+    recommended_books = recommended_books.merge(df[['title', 'thumbnail']], on='title', how='left')
+
+    return render_template("index.html", recommended_books=recommended_books, rank_type=rank_type)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app_flask.run(debug=True, use_reloader=False)
